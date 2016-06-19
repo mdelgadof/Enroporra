31680f8ae1b6c6e1382b3ab1a76b30f61186372f
Miguel Delgado <miguel.delgado@gmail.com>
2016-06-19 16:39:46 -0400

    Incluímos globales en apuesta.php para reconocer fase

--- apuesta.php
+++ apuesta.php
@@ -1,4 +1,5 @@
 <?
+include "inc/globales.php";
 if (date("Y-m-d H:i:s")>=$FECHA_APERTURA_PORRA_SEGUNDA_FASE || $_GET["forrest"]==1) $apuesta2=1; else $apuesta=1;
 /*$apuesta=1;*/
 include "index.php";
